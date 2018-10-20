ff0499160dddeebc9b8ea4f841babb04534b1d9c
Valdineia Signorelli <valsignorelli@Valdineias-MBP.fios-router.home>
2018-10-20 11:52:36 -0400

    create app

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendsArray = [
+	{
+		name: 'Pope Francis',
+		photo: 'http://www.mysticsofthechurch.com/2015/09/obedience-to-pope-being-faithful-to-god.html',
+		scores: [
+			'3',
+			'3',
+			'2',
+			'5',
+			'3',
+			'2',
+			'2',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'St. Francis of Assisi',
+		photo: 'http://stfrancischurch.net/wordpress/wp-content/uploads/2014/10/stFrancisAssisi.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Trump',
+		photo: 'http://ticklethewire.com/wp-content/uploads/2017/04/donald-trump-portrait.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+];
+
+module.exports = friendsArray;
